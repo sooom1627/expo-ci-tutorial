e22fad4d0a01b15bbef8a53d35e811d5e35b4bb5
soma <swfh2367@gmail.com>
2024-11-18 19:52:19 +0900

    perfome husky and lint staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,27 +2,31 @@
 const config = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: true,
+    project: "./tsconfig.json",
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "react", "react-native"],
   extends: [
-    // @see https://github.com/expo/expo/blob/main/packages/eslint-config-expo/.eslintrc.js
     "expo",
-    // @see https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/src/configs/recommended.ts
     "eslint:recommended",
-    // @see https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/src/configs/recommended-type-checked.ts
-    "plugin:@typescript-eslint/recommended-type-checked",
-    // @see https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/src/configs/stylistic-type-checked.ts
-    "plugin:@typescript-eslint/stylistic-type-checked",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-native/all"
   ],
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
   rules: {
-    // @see https://typescript-eslint.io/rules/array-type
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     "@typescript-eslint/array-type": "off",
-
-    // @see https://typescript-eslint.io/rules/consistent-type-definitions/
     "@typescript-eslint/consistent-type-definitions": "off",
-
-    // @see https://typescript-eslint.io/rules/consistent-type-imports/
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
@@ -30,42 +34,24 @@ const config = {
         fixStyle: "inline-type-imports",
       },
     ],
-
-    // @see https://typescript-eslint.io/rules/no-unused-vars/
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
-
-    // @see https://typescript-eslint.io/rules/require-await/
     "@typescript-eslint/require-await": "off",
-
-    // @see https://typescript-eslint.io/rules/no-misused-promises/
     "@typescript-eslint/no-misused-promises": [
       "error",
       {
         checksVoidReturn: { attributes: false },
       },
     ],
-
-    // https://typescript-eslint.io/rules/no-var-requires/
     "@typescript-eslint/no-var-requires": "off",
   },
   ignorePatterns: [
-    // 設定ファイル
     ".eslintrc.cjs",
     "babel.config.js",
     "metro.config.js",
-
-    // ビルド関連
     "dist",
     "build",
-
-    // 依存関係
     "node_modules",
-
-    // app-example関連
     "app-example",
     "app-example/**/*",
-
-    // その他
     "*.config.js",
     "*.config.cjs",
     "*.config.ts",
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,18 @@
-import { Text, View } from "react-native";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style={styles.container}>
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    flex: 1,
+    justifyContent: "center",
+  },
+});
